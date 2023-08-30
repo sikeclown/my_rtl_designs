187774dbcb664335d8f0d0e56a113a8d1bda2663
Chaitanya Kunda <114354982+sikeclown@users.noreply.github.com>
2023-08-31 01:03:17 +0530

    Update sync_dualport_ram.v

--- design/sync_dualport_ram.v
+++ design/sync_dualport_ram.v
@@ -61,16 +61,23 @@ end
 
 always @(*) begin
     case (wr_addr)
-       3'b000 :out_demux= 8'b00000001;
-       3'b001 :out_demux= 8'b00000010;
-       3'b010 :out_demux= 8'b00000100;
-       3'b011 :out_demux= 8'b00001000;
-       3'b100 :out_demux= 8'b00010000;
-       3'b101 :out_demux= 8'b00100000;
-       3'b110 :out_demux= 8'b01000000;
-       3'b111 :out_demux= 8'b10000000;
-       
-       default: out_demux= 8'bzzzzzzzz;
+       4'b0000 :out_demux= 16'b0000000000000001;
+       4'b0001 :out_demux= 16'b0000000000000010;
+       4'b0010 :out_demux= 16'b0000000000000100;
+       4'b0011 :out_demux= 16'b0000000000001000;
+       4'b0100 :out_demux= 16'b0000000000010000;
+       4'b0101 :out_demux= 16'b0000000000100000;
+       4'b0110 :out_demux= 16'b0000000001000000;
+       4'b0111 :out_demux= 16'b0000000010000000;
+       4'b1000 :out_demux= 16'b0000000100000000;
+       4'b1001 :out_demux= 16'b0000001000000000;
+       4'b1010 :out_demux= 16'b0000010000000000;
+       4'b1011 :out_demux= 16'b0000100000000000;
+       4'b1100 :out_demux= 16'b0001000000000000;
+       4'b1101 :out_demux= 16'b0010000000000000;
+       4'b1110 :out_demux= 16'b0100000000000000;
+       4'b1111 :out_demux= 16'b1000000000000000;
+       default: out_demux= 16'bz;
     endcase
 end
 
