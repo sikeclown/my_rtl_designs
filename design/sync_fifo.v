a0daede1fff2c47c377b9c33f7adfa981cfc7786
Chaitanya Kunda <114354982+sikeclown@users.noreply.github.com>
2023-09-01 02:45:45 +0530

    Update sync_fifo.v
    ignore previous commit and have that description for this commit.

--- design/sync_fifo.v
+++ design/sync_fifo.v
@@ -1,7 +1,7 @@
 module sync_fifo #(parameter width=8, depth=16, addr_size=4)(
     input [width-1:0] data_in,
     input rst, clk, rd_en, wr_en, 
-    output full, empty,
+    output fifo_full, fifo_empty,
     output [width-1:0] data_out
 );
     
@@ -9,13 +9,19 @@ reg [addr_size-1:0] rd_ptr, wr_ptr;
 
 reg [addr_size-1:0] rd,wr;
 
-assign full = (wr_ptr  == (rd_ptr+1))? 'b1:'b0;
-assign empty = (wr_ptr  == rd_ptr)? 'b1:'b0;
+assign fifo_full = (wr_ptr == depth-1)?'b1:'b0;
+assign fifo_empty = (wr_ptr == 'b0)?'b1:'b0;
+
+assign f = (wr_ptr < rd_ptr || fifo_full)? 'b1:'b0;
+assign e = (wr_ptr  == rd_ptr)? 'b1:'b0;
 
 // READ SIDE
 always @(*) begin
-    if (rd_en && !empty) begin
+    if (rd_en && !e) begin
         rd=rd_ptr+'d1;
+    end
+    else if (rd_en && e) begin
+        rd='b0;
     end
     else begin
         rd=rd_ptr;
@@ -35,8 +41,11 @@ sync_dualport_ram r1(data_in, clk, rst, wr_en, rd_en, wr_ptr, rd_ptr, data_out);
 
 //WRITE SIDE
 always @(*) begin
-    if (wr_en && !full) begin
+    if (wr_en && !f) begin
         wr=wr_ptr+'d1;
+    end
+    else if (wr_en && f) begin
+        wr='b0;
     end
     else begin
         wr=wr_ptr;
@@ -51,5 +60,4 @@ always @(posedge clk) begin
         wr_ptr<=wr;
     end
 end
-
 endmodule
