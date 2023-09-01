48d50cadf81d415d1c6786f9b83e5b2f98c4c5c6
Chaitanya Kunda <114354982+sikeclown@users.noreply.github.com>
2023-09-01 20:14:44 +0530

    Update sync_fifo.v
    change my argument about full and empty, the full and empty conditions in the previous commits arent great. changed the size of pointers where if the msb bits are same for wr and rd ptrs then they are in the same loop

--- design/sync_fifo.v
+++ design/sync_fifo.v
@@ -5,23 +5,25 @@ module sync_fifo #(parameter width=8, depth=16, addr_size=4)(
     output [width-1:0] data_out
 );
     
-reg [addr_size-1:0] rd_ptr, wr_ptr;
+reg [addr_size:0] rd_ptr, wr_ptr;
 
-reg [addr_size-1:0] rd,wr;
+reg [addr_size:0] rd,wr;
+wire f,e;
 
-assign fifo_full = (wr_ptr == depth-1)?'b1:'b0;
-assign fifo_empty = (wr_ptr == 'b0)?'b1:'b0;
+assign fifo_full = {wr_ptr[addr_size], wr_ptr[addr_size-1:0]} == {~rd_ptr[addr_size], rd_ptr[addr_size-1:0]} ? 'b1 : 'b0;
+assign fifo_empty = (wr_ptr == rd_ptr)?'b1:'b0;
 
-assign f = (wr_ptr < rd_ptr || fifo_full)? 'b1:'b0;
-assign e = (wr_ptr  == rd_ptr)? 'b1:'b0;
+assign f = fifo_full;
+//assign e = (wr_ptr  == rd_ptr)? 'b1:'b0;
+assign e = fifo_empty;
+
+
+
 
 // READ SIDE
 always @(*) begin
     if (rd_en && !e) begin
         rd=rd_ptr+'d1;
-    end
-    else if (rd_en && e) begin
-        rd='b0;
     end
     else begin
         rd=rd_ptr;
@@ -37,15 +39,12 @@ always @(posedge clk) begin
     end
 end
 
-sync_dualport_ram r1(data_in, clk, rst, wr_en, rd_en, wr_ptr, rd_ptr, data_out);
+sync_dualport_ram r1(data_in, clk, rst, wr_en, rd_en, wr_ptr[addr_size-1:0], rd_ptr[addr_size-1:0], data_out);
 
 //WRITE SIDE
 always @(*) begin
     if (wr_en && !f) begin
         wr=wr_ptr+'d1;
-    end
-    else if (wr_en && f) begin
-        wr='b0;
     end
     else begin
         wr=wr_ptr;
@@ -60,4 +59,5 @@ always @(posedge clk) begin
         wr_ptr<=wr;
     end
 end
+
 endmodule
