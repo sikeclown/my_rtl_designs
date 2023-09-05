29077d03c7bc5de23ab007e2e607bcb0f357fd05
Chaitanya Kunda <114354982+sikeclown@users.noreply.github.com>
2023-09-05 15:41:29 +0530

    Update async_singleport_ram.v

--- design/async_singleport_ram.v
+++ design/async_singleport_ram.v
@@ -1,51 +1,52 @@
 module async_singleport_ram #(parameter width = 16, depth = 8, addr_size = 3)(
-    inout [width-1:0]data_inout, 
+    inout [width-1:0]data, 
     input clk_wr, clk_rd, rst, wr_en,
     input [addr_size-1:0]addr
-    
 );
     
 reg [width-1:0] ram [depth-1:0]; 
 reg [width-1:0] temp_wr, data_out1;
 
-reg [depth-1:0] out_demux;
+reg [width-1:0] data_out, data_in;
 
-reg data;
+reg [depth-1:0] decode;
+
+assign data = !wr_en? data_out:'bz;
+
+
+always @(*) begin
+    if (wr_en) begin
+        data_in = data;
+    end
+    else begin
+        data_in = 'bz;
+    end
+end
+
+
 integer i;
-
-assign data_inout =wr_en ? 'bz : data;
 
 ///// READ OPERATION:
 
-
-/*always @(*) begin
-    if(!wr_en) begin
-        data_out1 = ram[addr];
-    end
-    else begin
-        data_out1=data;
-    end
-end
-*/
 //
 
 always @(posedge clk_rd) begin
     if(rst)begin
-        data<='b0;
+        data_out<='b0;
     end
-    else data<= data_out1;
+    else data_out<= data_out1;
 end
 
 ///// WRITE OPERATION:
 
 always @(*) begin
     if (wr_en) begin
-        temp_wr= data_inout;
-        data_out1=data;
+        temp_wr= data_in;
+        data_out1= data_out;
     end
     else begin
-        data_out1 = ram[addr];
-        temp_wr= ram[addr];
+        temp_wr = ram[addr];
+        data_out1= ram[addr];
     end
 end
 
@@ -55,7 +56,7 @@ always @(posedge clk_wr) begin
         if (rst) begin
             ram[i]<='b0;
         end
-        else if (out_demux[i]) begin
+        else if (decode[i]) begin
             ram[i]<=temp_wr;
         end
         else ram[i]<=ram[i];
@@ -65,16 +66,16 @@ end
 
 always @(*) begin
     case (addr)
-       3'b000 :out_demux= 8'b00000001;
-       3'b001 :out_demux= 8'b00000010;
-       3'b010 :out_demux= 8'b00000100;
-       3'b011 :out_demux= 8'b00001000;
-       3'b100 :out_demux= 8'b00010000;
-       3'b101 :out_demux= 8'b00100000;
-       3'b110 :out_demux= 8'b01000000;
-       3'b111 :out_demux= 8'b10000000;
+       3'b000 :decode= 8'b00000001;
+       3'b001 :decode= 8'b00000010;
+       3'b010 :decode= 8'b00000100;
+       3'b011 :decode= 8'b00001000;
+       3'b100 :decode= 8'b00010000;
+       3'b101 :decode= 8'b00100000;
+       3'b110 :decode= 8'b01000000;
+       3'b111 :decode= 8'b10000000;
        
-       default: out_demux= 8'bzzzzzzzz;
+       default: decode= 8'bzzzzzzzz;
     endcase
 end
 
