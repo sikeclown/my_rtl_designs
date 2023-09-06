aced33001744d1f82e2ef6adfd96691b7dbf2c23
Chaitanya Kunda <114354982+sikeclown@users.noreply.github.com>
2023-09-06 17:11:37 +0530

    sync_dualport_ram.v
    just change out_demux to decode, as this is the most appropriate name

--- design/sync_dualport_ram.v
+++ design/sync_dualport_ram.v
@@ -8,7 +8,7 @@ module sync_dualport_ram #(parameter width = 8, depth = 16, addr_size = 4)(
 reg [width-1:0] ram [depth-1:0]; 
 reg [width-1:0] temp_wr, data_out1;
 
-reg [depth-1:0] out_demux;
+reg [depth-1:0] decode;
 
 
 integer i;
@@ -51,7 +51,7 @@ always @(posedge clk) begin
         if (rst) begin
             ram[i]<='b0;
         end
-        else if (out_demux[i]) begin
+        else if (decode[i]) begin
             ram[i]<=temp_wr;
         end
         else ram[i]<=ram[i];
@@ -61,23 +61,23 @@ end
 
 always @(*) begin
     case (wr_addr)
-       4'b0000 :out_demux= 16'b0000000000000001;
-       4'b0001 :out_demux= 16'b0000000000000010;
-       4'b0010 :out_demux= 16'b0000000000000100;
-       4'b0011 :out_demux= 16'b0000000000001000;
-       4'b0100 :out_demux= 16'b0000000000010000;
-       4'b0101 :out_demux= 16'b0000000000100000;
-       4'b0110 :out_demux= 16'b0000000001000000;
-       4'b0111 :out_demux= 16'b0000000010000000;
-       4'b1000 :out_demux= 16'b0000000100000000;
-       4'b1001 :out_demux= 16'b0000001000000000;
-       4'b1010 :out_demux= 16'b0000010000000000;
-       4'b1011 :out_demux= 16'b0000100000000000;
-       4'b1100 :out_demux= 16'b0001000000000000;
-       4'b1101 :out_demux= 16'b0010000000000000;
-       4'b1110 :out_demux= 16'b0100000000000000;
-       4'b1111 :out_demux= 16'b1000000000000000;
-       default: out_demux= 16'bz;
+       4'b0000 :decode= 16'b0000000000000001;
+       4'b0001 :decode= 16'b0000000000000010;
+       4'b0010 :decode= 16'b0000000000000100;
+       4'b0011 :decode= 16'b0000000000001000;
+       4'b0100 :decode= 16'b0000000000010000;
+       4'b0101 :decode= 16'b0000000000100000;
+       4'b0110 :decode= 16'b0000000001000000;
+       4'b0111 :decode= 16'b0000000010000000;
+       4'b1000 :decode= 16'b0000000100000000;
+       4'b1001 :decode= 16'b0000001000000000;
+       4'b1010 :decode= 16'b0000010000000000;
+       4'b1011 :decode= 16'b0000100000000000;
+       4'b1100 :decode= 16'b0001000000000000;
+       4'b1101 :decode= 16'b0010000000000000;
+       4'b1110 :decode= 16'b0100000000000000;
+       4'b1111 :decode= 16'b1000000000000000;
+       default: decode= 16'bz;
     endcase
 end
 
